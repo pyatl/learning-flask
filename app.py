2f3cbf1f754e2b71999c11450960c93ac7171f2d
pryelluw <pryelluw@gmail.com>
2020-09-22 20:34:22 -0400

    add templates

--- app.py
+++ app.py
@@ -1,22 +1,10 @@
-from flask import Flask
+from flask import (
+    Flask,
+    render_template
+)
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-@app.route('/cheese')
-def cheese():
-    return 'say cheese'
-
-
-@app.route('/hello/<name>')
-def say_hello(name):
-    return f'Hello, {name}'
-
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    return f'post id: {post_id}'
+@app.route('/<name>')
+def index(name):
+    return render_template('index.html', name=name)
