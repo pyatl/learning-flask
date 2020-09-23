48a90b7f542af5d7e862674e166bf2a2426d6bfd
pryelluw <pryelluw@gmail.com>
2020-09-22 20:25:09 -0400

    simple views

--- app.py
+++ app.py
@@ -6,3 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/cheese')
+def cheese():
+    return 'say cheese'
+
+
+@app.route('/hello/<name>')
+def say_hello(name):
+    return f'Hello, {name}'
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f'post id: {post_id}'
